f72ddb98c824a8268b4579dc0a984e6b08660d88
Jonathan Schaul <46248679+jonsch1@users.noreply.github.com>
2025-11-12 05:49:24 +0100

    cleaned up old code

--- omnipath_build/utils/__init__.py
+++ omnipath_build/utils/__init__.py
@@ -1,13 +1,5 @@
 """Utilities for parquet-based pipeline."""
 
-from .constants import get_database_path
-from .exceptions import (
-    BronzeLoaderError,
-    GoldLoaderError,
-    LoaderError,
-    OmniPathError,
-    SilverLoaderError,
-)
 from .logging_utils import log_execution_time
 from .path_manager import PathManager
 
@@ -15,12 +7,5 @@ from .path_manager import PathManager
 __all__ = [
     'PathManager',
     'get_database_path',
-    # Logging
     'log_execution_time',
-    # Exceptions
-    'OmniPathError',
-    'LoaderError',
-    'BronzeLoaderError',
-    'SilverLoaderError',
-    'GoldLoaderError',
 ]
