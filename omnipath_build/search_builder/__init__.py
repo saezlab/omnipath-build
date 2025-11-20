33ea47c3d329eee09699fca573b0dc9d667fb456
Jonathan Schaul <46248679+jonsch1@users.noreply.github.com>
2025-11-20 06:09:42 +0100

    .

--- omnipath_build/search_builder/__init__.py
+++ omnipath_build/search_builder/__init__.py
@@ -1,5 +1,6 @@
-"""Search entity builder - aggregates global tables into Meilisearch documents."""
+"""Search builders for Meilisearch documents."""
 
 from .build_search_entities import build_search_entities
+from .build_search_interactions import build_search_interactions
 
-__all__ = ["build_search_entities"]
+__all__ = ["build_search_entities", "build_search_interactions"]
